f643653ceef451b65f9f5b26314ad648d00442e8
João Rebelo Pires <joaorgpires@gmail.com>
2017-04-16 21:25:05 +0100

    Add main.cpp trie.cpp trie.h

---
+++ src/trie.h
@@ -0,0 +1,17 @@
+// For now, let us implement this only for strings
+// Each node will have a boolean marking it as a leaf (or not)
+// In order to get everything on trie, we could probably do a dfs and get a vector of strings
+
+#ifndef trie_h
+#define trie_h
+
+class trie {
+public:
+
+private:
+  struct TrieNode;
+
+
+};
+
+#endif
